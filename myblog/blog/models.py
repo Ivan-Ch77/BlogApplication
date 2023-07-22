2b77925d3c0671f1aa511c0c0b4e0fd3a0e26d8c
Иван Чернядьев <chim20r077@student.bmstu.ru>
2023-07-23 00:43:04 +0300

    Added the ability to share articles by mail, rewritten post_list as a class-based view

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 # Add the custom manager which retrieve all posts that have a PUBLISHED status
@@ -21,7 +22,8 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     # We will use the slug field to build beautiful, SEO-friendly URLs for blog posts
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_post')
@@ -46,3 +48,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'testappblogapplication@gmail.com'
+EMAIL_HOST_PASSWORD = 'cqsawcsxwlsnpkux'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
